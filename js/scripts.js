d1c22058b4bab9fa0b4c5fb2ff3e00c2fbd2e908
Zizoubrown <zizumoha2015@gmail.com>
2020-05-22 00:16:39 +0300

    add changes to script.js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,49 @@
+let myArray = []
+
+function choose() {
+  myArray.length = 0;
+
+  if (document.getElementById("djay").checked) {
+
+    myArray.push(20000)
+    document.getElementById("system").innerHTML = ("Dj and System Ksh: 20000")
+  }
+  if (document.getElementById("food").checked) {
+
+    myArray.push(30000)
+    document.getElementById("fud").innerHTML = ("Food Ksh: 30000")
+
+  }
+  if (document.getElementById("venue").checked) {
+
+    myArray.push(30000)
+    document.getElementById("venu").innerHTML = ("venue Ksh: 30000")
+  }
+}
+function basic() {
+  alert("you have chosen the Basic package at ksh 35000")
+  document.getElementById("base").disabled = true;
+  document.getElementById("intermediate").disabled = true;
+  document.getElementById("premium").disabled = true;
+}
+function inter() {
+  alert("you have chosen the Intermediate package at ksh 65000")
+  document.getElementById("base").disabled = true;
+  document.getElementById("premium").disabled = true;
+  document.getElementById("intermediate").disabled = true;
+}
+function prem() {
+  alert("you have chosen the Premium package at ksh 85000")
+  document.getElementById("premium").disabled = true;
+  document.getElementById("intermediate").disabled = true;
+  document.getElementById("base").disabled = true;
+}
 $(document).ready(function () {
   $("form#custom").submit(function (event) {
     event.preventDefault();
     drinks = $("input[type=checkbox]:checked").map(function (_, sel) {
         return $(sel).val();
- })
+      })
       .get();
 
     if (drinks === undefined || drinks.length == 0) {
@@ -31,40 +71,3 @@ $(document).ready(function () {
   });
   
 });
-let myArray = [];
-
-function choose(){
-    myArray.length = 0;
-
-    if(document.getElementById("djay").checked){
-        myArray.push(20000)
-        document.getElementById("system").innerHTML = ("Dj and System Ksh. 20000")
-    };
-    if (document.getElementById("food").checked){
-        myArray.push(30000)
-        document.getElementById("fud").innerHTML = ("Food Ksh. 30000")
-    };
-    if (document.getElementById("venue").checked){
-        myArray.push(30000)
-        document.getElementById("venu").innerHTML = ("Venue Ksh. 30000")
-    };
-};
-
-function basic(){
-    alert("You have chosen the Basic package at ksh 35000")
-    document.getElementById("base").disabled = true;
-    document.getElementById("intermediate").disabled = true;
-    document.getElementById("premium").disabled = true;
-};
-function inter(){
-    alert("You have chosen the intermidiate package at ksh 65000")
-    document.getElementById("base").disabled = true;
-    document.getElementById("intermediate").disabled = true;
-    document.getElementById("premium").disabled = true;
-};
-function prem(){
-    alert("You have chosen the Premium package at ksh 85000")
-    document.getElementById("base").disabled = true;
-    document.getElementById("intermediate").disabled = true;
-    document.getElementById("premium").disabled = true;
-};
