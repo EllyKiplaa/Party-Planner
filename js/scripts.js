d059a276e61751b79933bd30315b81a7325a29c4
EllyKiplaa <ellykiplangat200@gmail.com>
2020-05-21 23:31:01 +0300

    Arrange the scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,3 @@
-$(document).ready(function () {
-  $("form#custom").submit(function (event) {
-    event.preventDefault();
-    drinks = $("input[type=checkbox]:checked").map(function (_, sel) {
-        return $(sel).val();
- })
-      .get();
-
-    if (drinks === undefined || drinks.length == 0) {
-      alert("make selections please");
-      return false;
-    } else {
-      $("#drinks").html("")
-      drinks.forEach(drink => {
-        $("#drinks").append("<li class='drink'>" + drink + "</li>");
-      });
-    };
-
-    let security = $("#select option:selected").text();
-    let securityCost = parseInt($("#select option:selected").val())
-
-    $("#security").text(security);
-    myArray.push(securityCost)
-
-    let total = 0;
-
-    myArray.forEach(n => {
-      total += n
-      $("#total").text(total)
-    });
-  });
-  
-});
 let myArray = [];
 
 function choose(){
@@ -70,3 +37,37 @@ function prem(){
     document.getElementById("intermediate").disabled = true;
     document.getElementById("premium").disabled = true;
 };
+
+$(document).ready(function () {
+  $("form#custom").submit(function (event) {
+    event.preventDefault();
+    drinks = $("input[type=checkbox]:checked").map(function (_, sel) {
+        return $(sel).val();
+ })
+      .get();
+
+    if (drinks === undefined || drinks.length == 0) {
+      alert("make selections please");
+      return false;
+    } else {
+      $("#drinks").html("")
+      drinks.forEach(drink => {
+        $("#drinks").append("<li class='drink'>" + drink + "</li>");
+      });
+    };
+
+    let security = $("#select option:selected").text();
+    let securityCost = parseInt($("#select option:selected").val())
+
+    $("#security").text(security);
+    myArray.push(securityCost)
+
+    let total = 0;
+
+    myArray.forEach(n => {
+      total += n
+      $("#total").text(total)
+    });
+  });
+  
+});
