a227706f8e176f1c6664752a1d1073a25779b446
EllyKiplaa <ellykiplangat200@gmail.com>
2020-05-21 22:09:06 +0300

    Solve the bug at the js

--- js/scripts.js
+++ js/scripts.js
@@ -34,21 +34,23 @@ $(document).ready(function () {
 let myArray = [];
 
 function choose(){
-    myArray.length = 0;
+    myArray.length = 10;
 
     if(document.getElementById("djay").checked){
         myArray.push(20000)
-        document.getElementById("system").innerHTML = ("Dj and System Ksh. 20000")
+        document.getElementById("system").innerHTML = ("Dj and System Ksh:20000")
     };
+
     if (document.getElementById("food").checked){
         myArray.push(30000)
-        document.getElementById("fud").innerHTML = ("Food Ksh. 30000")
+        document.getElementById("fud").innerHTML = ("Food Ksh:30000")
+
     };
     if (document.getElementById("venue").checked){
         myArray.push(30000)
-        document.getElementById("venu").innerHTML = ("Venue Ksh. 30000")
-    };
-};
+        document.getElementById("venu").innerHTML = ("Venue Ksh:30000")
+    }
+}
 
 function basic(){
     alert("You have chosen the Basic package at ksh 35000")
