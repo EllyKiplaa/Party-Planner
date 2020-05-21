e268b911fcb2267cfa26175c0ffb5c290d8c2712
Zizoubrown <zizumoha2015@gmail.com>
2020-05-21 12:03:12 +0300

    add function code

--- js/scripts.js
+++ js/scripts.js
@@ -3,5 +3,8 @@ let myArray = [];
 function choose(){
     myArray.length = 0;
 
-    if(document.getElementById("djay").checked)
+    if(document.getElementById("djay").checked){
+        myArray.push(20000)
+        document.getElementById("system").innerHTML = ("Dj and System Ksh. 20000")
+    };
 };
