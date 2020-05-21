07f5705d679274487b1ee3160be6f7c3d4879bd3
EllyKiplaa <ellykiplangat200@gmail.com>
2020-05-21 13:28:32 +0300

    Add the functions and the method .map to so that the array value is added to the input of the drinks selection

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
 $(document).ready(function () {
     $("form#custom").submit(function (event) {
-      
+      event.preventDefault();
+      drinks = $("input[type=checkbox]:checked").map(function (_, sel) {
+          return $(sel).val();
+        })
+        .get();
