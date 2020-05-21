10c46f4593d88c4c33fed65ad052d07f5e619a26
EllyKiplaa <ellykiplangat200@gmail.com>
2020-05-21 13:33:27 +0300

    Add a condition statement of if the selection of if either the selection of the drinks are made it will return false and the else statement

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,15 @@
 $(document).ready(function () {
-    $("form#custom").submit(function (event) {
-      event.preventDefault();
-      drinks = $("input[type=checkbox]:checked").map(function (_, sel) {
-          return $(sel).val();
-        })
-        .get();
+  $("form#custom").submit(function (event) {
+    event.preventDefault();
+    drinks = $("input[type=checkbox]:checked").map(function (_, sel) {
+        return $(sel).val();
+      })
+      .get();
+
+      if (drinks === undefined || drinks.length == 0) {
+        alert("make selections please");
+        return false;
+      } else {
+
+      });
+    });
